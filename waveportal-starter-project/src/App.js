2fdc380a68cc48dea69531478add07149741fd70
raynac <ryan.aaixe@gmail.com>
2022-08-11 01:54:31 +0800

    changed bg color

--- waveportal-starter-project/src/App.js
+++ waveportal-starter-project/src/App.js
@@ -5,25 +5,27 @@ import './App.css';
 export default function App() {
 
   const wave = () => {
-    
+
   }
-  
+
   return (
-    <div className="mainContainer">
+    <body>
+      <div className="mainContainer">
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there! 👋
+        <div className="dataContainer">
+          <div className="header">
+            👋 Hey there! 👋
+          </div>
+
+          <div className="bio">
+            I am a software engineer and a web developer. I am currently working at <a href="https://www.waveportal.com">WavePortal</a> as a software engineer.
+          </div>
+
+          <button className="waveButton" onClick={wave}>
+            Wave at Me
+          </button>
         </div>
-
-        <div className="bio">
-        I am a software engineer and a web developer. I am currently working at <a href="https://www.waveportal.com">WavePortal</a> as a software engineer.
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
       </div>
-    </div>
+    </body>
   );
 }
